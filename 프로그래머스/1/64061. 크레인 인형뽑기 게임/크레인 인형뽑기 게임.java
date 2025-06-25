532f7745ce1c5e0e95ec25eac0ea907cb4545ddd
eunhyeon5322 <90364682+eunhyeon5322@users.noreply.github.com>
2025-06-25 14:01:40 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.97 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        Stack<Integer> choose = new Stack<>();
+        Stack<Integer> check = new Stack<>();
+        int count = 0;
+        
+        for(int i=0; i<moves.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[j][moves[i]-1] != 0){
+                    choose.push(board[j][moves[i]-1]);
+                    board[j][moves[i]-1]=0;
+                    break;
+                }
+            }
+            
+        }
+        
+        while(!choose.isEmpty()){
+            if(check.isEmpty()){
+                check.push(choose.pop());
+                continue;
+            }
+            if(choose.peek() == check.peek()){
+                    choose.pop();
+                    check.pop();
+                    count += 1;
+            }else{
+                check.push(choose.pop());
+            }
+        }
+        return (count * 2);
+
+    }    
+}
