cf658d7cae03d8d386fe70713b3fce6caa88e60b
eunhyeon5322 <90364682+eunhyeon5322@users.noreply.github.com>
2025-07-03 10:15:10 +0900

    [level 3] Title: 합승 택시 요금, Time: 2488.30 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413) 
 
 ### 성능 요약
 
-메모리: 11.9 MB, 시간: 2514.29 ms
+메모리: 11.8 MB, 시간: 2488.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 01일 15:42:01
+2025년 07월 03일 10:15:08
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,23 +1,22 @@
 def solution(n, s, a, b, fares):
+    #무한 정의
     INF = int(1e9)
-    graph = [[INF] * (n + 1) for _ in range(n + 1)]
-
-    for i in range(1, n + 1):
-        graph[i][i] = 0
-
-    for c, d, f in fares:
+    #graph 만들고
+    graph = [[INF] * (n+1) for _ in range(n+1)]
+    #graph 초기화
+    for i in range(1, n+1):
+        graph[i][i]=0
+    #graph에 fares 값 넣고
+    for c,d,f in fares:
         graph[c][d] = f
         graph[d][c] = f
-
-    # 플로이드-워셜
-    for k in range(1, n + 1):
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-
-    # 모든 경유지 i에 대해 s → i → a + i → b 비용 계산
+    #3중 for문 min 
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                graph[i][j]=min(graph[i][j], graph[i][k]+graph[k][j])
+    #answer min 값 구하기
     answer = INF
-    for i in range(1, n + 1):
-        answer = min(answer, graph[s][i] + graph[i][a] + graph[i][b])
-
+    for i in range(1, n+1):
+        answer=min(answer, graph[s][i]+graph[i][a]+ graph[i][b])
     return answer
