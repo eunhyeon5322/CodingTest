2aa566e718e8745074373167e4578f0927e7dce6
eunhyeon5322 <90364682+eunhyeon5322@users.noreply.github.com>
2025-10-28 23:24:11 +0900

    [level 0] Title: 카운트 업, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -1,5 +1,2 @@
 def solution(start_num, end_num):
-    result=[]
-    for i in range(start_num, end_num+1):
-        result.append(i)
-    return result
+    return [i for i in range(start_num, end_num+1)]
